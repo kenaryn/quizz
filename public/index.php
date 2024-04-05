d717f88c3feee6613dc1138afd38bf4e872608b3
aurele <aurelien.plazzotta@tutanota.com>
2024-04-05 16:21:29 +0200

    Adding connexion to DB, copying DB requests script and adapting yaml file according to new PDO object

--- public/index.php
+++ public/index.php
@@ -9,3 +9,15 @@ use Entities\Quizz\Quizz;
 $json = file_get_contents('./quizz.json');
 $json_data = json_decode($json);
 $quizz = Quizz::create($json_data);
+
+try {
+  $connexion = new PDO('mysql:host=mysql-srv;dbname=quizz_db','kena','IAmFreeNow!411');
+  $stmt = $connexion->prepare('select * from Quizz');
+  $stmt->execute();
+  while ($row = $stmt->fetch()) {
+    print_r($row);
+  }
+} catch (PDOException $e) {
+  echo "error: " . $e->getMessage() . ', error code: ' . $e->getCode(), PHP_EOL;
+  echo 'The error occured in . ' . $e->getFile() . ' at line '. $e->getLine(), PHP_EOL;
+}
